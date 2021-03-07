03b5434745708782fcaf199592f446cf8a9cdfa8
Vinicius Correa <viniciushenriquegc@unifei.edu.br>
2021-03-07 12:40:18 -0300

    1.0.0 - Rotas configuradas, Nav-Bar das paginas criadas.
    AS rotas de todas as paginas foram configuradas de acordo com a Navigation Bar, presente em todas elas

--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,38 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/',(req,res)=>{
+router.get('/teste',(req,res)=>{
     res.send('It works');
 });
 
-router.get('/almofa',(req,res)=>{
-    res.send('Almofadinhas');
-});
-
 //Testando pagina com carrosel feito com o pug
-router.get('/form',(req,res)=>{
+router.get('/mylayout',(req,res)=>{
     res.render('mylayout');
 });
 
-//Pagina home
+//Pagina home(parte 1)
+router.get('/',(req,res)=>{
+    res.render('home');
+});
+
+//Pagina home(parte 2)
 router.get('/home',(req,res)=>{
     res.render('home');
+});
+
+//Pagina Sobre(about)
+router.get('/about',(req,res)=>{
+    res.render('about');
+});
+
+//Pagina de planos oferecidos)
+router.get('/plans',(req,res)=>{
+    res.render('plans');
+});
+
+//Pagina de contato do cliente
+router.get('/contact',(req,res)=>{
+    res.render('contact');
 });
 
 module.exports = router;
