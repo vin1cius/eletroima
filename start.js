4484ae649b1771d9a5dede25f5ee4c9fe733e735
Vinicius Correa <viniciushenriquegc@unifei.edu.br>
2021-03-10 19:25:43 -0300

    1.1.0 - Programando a conexao com o banco de dados
    Foi programado a conexao com o mongodb na plataforma atlas, onde seram criadas colecoes e schemas para o frontend

--- start.js
+++ start.js
@@ -1,5 +1,26 @@
 //Apenas inicia o servidor
 const app = require('./app');
+const mongoose = require('mongoose');
+require('dotenv/config')
+
+//Conectando ao banco de dados
+
+//opções de conexão
+//https://dev.to/dalalrohit/how-to-connect-to-mongodb-atlas-using-node-js-k9i
+const connectionParams={
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true 
+}
+
+mongoose.connect(process.env.DB_CONNECTION,connectionParams)
+    .then( () => {
+        console.log('Conectado ao banco de dados! ')
+    })
+    .catch( (err) => {
+        console.error(`Erro na conexão!!!. \n${err}`);
+    })
+
 const server = app.listen(3000,() => {
     console.log(`Express is running on port ${server.address().port}`);
 });
